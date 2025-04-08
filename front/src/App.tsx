97991161f524721fc0b08c9c20b2edcdb07e8458
Mahay <hirimanana@yahoo.fr>
2025-04-08 16:33:58 +0400

    set App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
+import FileUpload from './components/fileupload/FileUpload'
+import OutputBox from './components/outbutbox/OutputBox';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [extractedText, setExtractedText] = useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="max-w-2xl mx-auto mt-10 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">📝 DeepScribe</h1>
+      <FileUpload onTextExtract={setExtractedText} />
+      {extractedText && <OutputBox text={extractedText} />}
+    </div>
+  );
 }
 
 export default App
