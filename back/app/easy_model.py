b3d12e37670d610e52df04280ca5a042a6193549
Mahay <hirimanana@yahoo.fr>
2025-04-10 17:11:09 +0400

    update easyOCR

--- back/app/easy_model.py
+++ back/app/easy_model.py
@@ -1,13 +1,14 @@
 import easyocr
-from PIL import Image
 
-# Load EasyOCR Reader (you can specify multiple languages like ['en', 'fr'] if needed)
-reader = easyocr.Reader(['en'], gpu=True)  # Set gpu=False if you don't have CUDA
+reader = easyocr.Reader(['en'])  # use gpu=False if needed
 
 def extract_text_from_image(image_path: str) -> str:
-    print("[DEBUG] Loading image for OCR:", image_path)
-    results = reader.readtext(image_path, detail=0)
-    
-    extracted_text = "\n".join(results)
-    print("[DEBUG] Extracted text:", extracted_text)
-    return extracted_text
+    print("[DEBUG] Reading image at:", image_path)
+    try:
+        results = reader.readtext(image_path, detail=2)
+        extracted_text = "\n".join([item[1] for item in results])
+        print("[DEBUG] Extracted:", extracted_text)
+        return extracted_text
+    except Exception as e:
+        print("[ERROR] OCR failed:", str(e))
+        return "OCR extraction failed"
--- back/app/main.py
+++ back/app/main.py
@@ -4,6 +4,7 @@ from typing import Dict
 import shutil
 import os
 from .easy_model import extract_text_from_image
+import uuid
 
 app = FastAPI()
 
@@ -21,7 +22,9 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.post("/upload")
 def upload_file(file: UploadFile = File(...)) -> Dict[str, str]:
-    temp_path = os.path.join(UPLOAD_DIR, file.filename)
+    #generate safe filename, replace spaces, accents and file name can be any format
+    safe_filename = f"{uuid.uuid4().hex}-{file.filename.replace(' ', '_').replace('.', '_')}"
+    temp_path = os.path.join(UPLOAD_DIR, safe_filename)
 
     with open(temp_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
